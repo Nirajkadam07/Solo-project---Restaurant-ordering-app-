bd4896ecaeecbb06510e1e7d17079f19bf50649e
Nirajkadam07 <64134180+Nirajkadam07@users.noreply.github.com>
2025-09-09 09:58:17 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    id: 0,
+    price: 14,
+    image: "images/pizza.png",
+    altText: "Pizza photo",
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["chicken", "cheese", "lettuce"],
+    price: 12,
+    image: "images/hamburger.png",
+    id: 1,
+    altText: "Hamburger photo",
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 16,
+    image: "images/beer.png",
+    id: 2,
+    altText: "Beer photo",
+  },
+];
